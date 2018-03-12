23ced354ab6c43760188999b2407ba7c75adc02f
unknown <xplacht@ad.fi.muni.cz>
2018-03-12 11:44:59 +0100

    calibration room created

---
+++ Assets/Scripts/xx_calibrating_room/CalibratingRoomLevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CalibratingRoomLevelManager : MonoBehaviour {
+
+    enum MenuMode { Nothing, NewGame, ContinueGame, ExitGame }
+
+    public static CalibratingRoomLevelManager Instance;
+
+    [SerializeField]
+    private GameObject Teleport;
+
+    private MenuMode _menuMode = MenuMode.Nothing;
+
+    private void Start()
+    {
+        Teleport.SetActive(false);
+    }
+
+    public void PrepareTeleportNewGame()
+    {
+        Teleport.SetActive(true);
+        _menuMode = MenuMode.NewGame;
+    }
+
+    public void PrepareTeleportContinueGame()
+    {
+        Teleport.SetActive(false);
+        _menuMode = MenuMode.ContinueGame;
+    }
+
+    public void TeleportPlayer()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    private void OnValidate()
+    {
+        if (Instance != null)
+            GameObject.Destroy(Instance);
+        else
+            Instance = this;
+
+        //DontDestroyOnLoad(this);
+    }
+}
