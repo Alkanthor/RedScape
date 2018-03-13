43003e09e28e3a535fabd574cd714a7d8c35b08e
unknown <xplacht@ad.fi.muni.cz>
2018-03-13 11:05:20 +0100

    add infoboard prefab, add headset collision position rewind

---
+++ Assets/Scripts/ScreenScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScript : MonoBehaviour {
+
+    private int actualScreen;
+
+    private int screenCount;
+
+    private RawImage boardImage;
+
+    private Object[] textures;
+
+
+	// Use this for initialization
+	void Start () {
+        actualScreen = 1;
+        textures = Resources.LoadAll("Textures", typeof(Texture));
+        screenCount = textures.Length;
+
+        boardImage = GameObject.Find("InfoBoardImage").GetComponent<RawImage>();
+        boardImage.texture = (Texture)textures[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextScreen()
+    {
+        Debug.Log("Next");
+        if(actualScreen<screenCount)
+        {
+            actualScreen++;
+        }
+        boardImage.texture = (Texture)textures[actualScreen - 1];
+    }
+
+    public void PrevScreen()
+    {
+        Debug.Log("Prev");
+        if (actualScreen > 1)
+        {
+            actualScreen--;
+        }
+        boardImage.texture = (Texture)textures[actualScreen - 1];
+    }
+}
