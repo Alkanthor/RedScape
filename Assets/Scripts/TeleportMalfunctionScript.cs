a48b7278ef5b62ded9769c5ca9e082127d5bc454
unknown <xgluszny@ad.fi.muni.cz>
2018-03-15 13:33:01 +0100

    script for detection teleport

---
+++ Assets/Scripts/TeleportMalfunctionScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TeleportMalfunctionScript : MonoBehaviour {
+
+    private VRTK_HeightAdjustTeleport VRTKTeleportScript;
+
+    private bool playerTeleportedOuside = false;
+
+    // Use this for initialization
+    void Start () {
+        VRTKTeleportScript = GameObject.Find("PlayArea").GetComponent<VRTK_HeightAdjustTeleport>();
+        VRTKTeleportScript.Teleported += PlayerTeleported;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayerTeleported(object sender, DestinationMarkerEventArgs e)
+    {
+        Debug.Log("PlayerTeleported");
+        if(playerTeleportedOuside)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
