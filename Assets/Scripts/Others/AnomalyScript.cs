c2552f2b86eead01d8d76e7644f8eb6c73883cc7
Alkanthor <rogls@seznam.cz>
2018-05-31 13:07:02 +0200

    anomaly script started

---
+++ Assets/Scripts/Others/AnomalyScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomalyScript : MonoBehaviour
+{
+    public GameObject targetAnomaly;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (this.Name == "anomaly_a")
+        {
+            targetAnomaly = this.Transform.Parent.Find("anomaly_b");
+        }
+        else
+        {
+            targetAnomaly = this.Transform.Parent.Find("anomaly_a");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider item)
+    {
+        if (item.Tag=="Teleportable")
+        {
+            item.transform.position = targetAnomaly.transform.position;
+        }
+    }
+}
