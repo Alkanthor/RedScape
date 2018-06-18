87c470eb71a7c2b2729aadc95f2b6ee888041b1f
dadloo <daniela.plachtova@gmail.com>
2018-06-18 23:39:17 +0200

    car puzzle

---
+++ Assets/Scripts/02_social_room/CardAttach.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class CardAttach : MonoBehaviour {
+
+    private GameObject _attachTo;
+    public bool Attach;
+
+    private void Start()
+    {
+        this.GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += OnCardGrabbed;
+    }
+
+    private void OnCardGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        Attach = false;
+    }
+
+
+    public void SetAttachTo(GameObject attachTo)
+    {
+        _attachTo = attachTo;
+    }
+    private void Update()
+    {
+        if(Attach)
+        {
+            this.transform.position = _attachTo.transform.position;
+        }
+    }
+
+}
